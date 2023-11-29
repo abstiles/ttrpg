22fdd696a455a801c770f62b7f3b65b37fba4e53
Anjou Stiles <anjou.stiles@gmail.com>
2023-11-28 21:23:00 -0600

    Avoid double-activate

--- js/settings.js
+++ js/settings.js
@@ -28,32 +28,40 @@
 		// Save updated setting whenever an input is changed.
 		settingsControls.forEach((item, index) => {
 			const activate = function() {
+				item.setAttribute('data-activated', 'true')
+				document.documentElement.setAttribute(
+					`data-${item.name}`, item.value
+				);
+				localStorage.setItem(item.name, item.value);
+				// Strip activated attribute off other radio buttons.
+				settingsControls.forEach((item2, index2) => {
+					if (index !== index2) {
+						item2.removeAttribute('data-activated');
+					}
+				});
+			}
+			item.addEventListener('keydown', event => {
+				if (event.code === 'Space') {
+					// I don't quite understand the asymmetry between this and the
+					// "click" listener. If they both have the same logic, it's like
+					// they're both triggered when attempting to deactivate, but the same
+					// behavior doesn't happen during activation.
+					item.checked = !item.checked;
+					if (item.checked) {
+						// Going from nothing checked to something checked.
+						activate();
+					}
+				}
+			});
+			item.addEventListener('click', () => {
 				if (!item.getAttribute('data-activated')) {
-					item.setAttribute('data-activated', 'true')
-					document.documentElement.setAttribute(
-						`data-${item.name}`, item.value
-					);
-					localStorage.setItem(item.name, item.value);
-					// Strip activated attribute off other radio buttons.
-					settingsControls.forEach((item2, index2) => {
-						if (index !== index2) {
-							item2.removeAttribute('data-activated');
-						}
-					});
+					activate();
 				} else {
 					item.removeAttribute('data-activated');
 					document.documentElement.removeAttribute(`data-${item.name}`);
 					localStorage.removeItem(item.name);
 					item.checked = false;
 				}
-			}
-			item.addEventListener('keydown', event => {
-				if (event.code === 'Space') {
-					activate();
-				}
-			});
-			item.addEventListener('click', () => {
-				activate();
 			});
 		});
 	};
